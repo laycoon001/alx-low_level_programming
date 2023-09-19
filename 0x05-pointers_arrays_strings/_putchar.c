9484968d539e45a772225d7e3315bf7345c794c3
laycoon001 <ademola0457@gmail.com>
2023-09-18 17:59:06 -0700

    kill

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success, the character is returned. On error, -1 is returned.
+ */
+int _putchar(char c)
+{
+	return write(1, &c, 1);
+}
+
